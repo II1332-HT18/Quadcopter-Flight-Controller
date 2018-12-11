cb90e608053caa2200fd2f3de7154b0a790f93e7
Elias <echahine@kth.se>
2018-12-11 09:52:06 +0100

    Fixed files.
    Changed filter file so that filter yaw is working correctly.

--- Inc/Filter.h
+++ Inc/Filter.h
@@ -25,6 +25,7 @@ typedef struct {
   
   float acc_pitch;  //Used for the calculated (arctan2) acc_pitch
   float acc_roll;   //Used for the calculated (arctan2) acc_roll
+  //float acc_yaw;   //Used for the calculated (arctan2) acc_yaw, added 2018-12-07
   
   
   float filter_pitch;  //Used for complement filtered pitch
@@ -32,21 +33,22 @@ typedef struct {
   float filter_yaw;    //Used for complement filtered yaw
   
   // 2018-11-2X added space to send RAW acc data
-  float raw_acc_x;  //New var used for the RAW data acc_x
-  float raw_acc_y;  //New var used for the RAW data acc_y
-  float raw_acc_z;  //New var used for the RAW data acc_z
+//  float raw_acc_x;  //New var used for the RAW data acc_x
+//  float raw_acc_y;  //New var used for the RAW data acc_y
+//  float raw_acc_z;  //New var used for the RAW data acc_z
   
   // 2018-12-06 added space for 'change speed' since the control-group needs it
-  float pitch_angle_speed;    // New var used for speed of change, the derivative
-  float roll_angle_speed;     // New var used for speed of change, the derivative
-  float yaw_angle_speed;      // New var used for speed of change, the derivative
+  //float pitch_angle_speed;    // New var used for speed of change, the derivative
+  //float roll_angle_speed;     // New var used for speed of change, the derivative
+  //float yaw_angle_speed;      // New var used for speed of change, the derivative
   
 } FILTER_complement_struct;
 
 // Struct to store old and current value in order to calculate angel speed
 typedef struct {
   float old;            // Holds old value
-//  float current;        // Holds the current value
+  float older;        // Holds older value in order to calc roling mean value
+  float oldest;         //Holds oldest value
 } FILTER_angle_speed_struct;
   
 
